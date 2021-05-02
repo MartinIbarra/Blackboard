4cdd7c63dd8fe4c045fd575ba2ff72102d081d78
ibarraMartin <ibarramartin92@gmail.com>
2021-05-02 20:35:47 -0300

    local env

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.CONNECT_DB_URL, { useNewUrlParser: true, useUnified
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const corsOptions = {
-    origin: 'https://blackboard-app.netlify.app',
+    origin: 'http://localhost:3000',
     credentials: true,
     optionsSuccessStatus: 200
 }
